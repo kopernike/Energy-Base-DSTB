ba84c9b6d5dfea944b9fc5be3761b60cf99f5c22
kopernike <wi23b052@technikum-wien.at>
2025-04-23 17:19:00 +0200

    kleine Verbesserung

--- GUI_JAVAFX/src/main/java/com/example/gui_javafx/EnergyGuiController.java
+++ GUI_JAVAFX/src/main/java/com/example/gui_javafx/EnergyGuiController.java
@@ -1,46 +1,3 @@
-/* package com.example.gui_javafx;
-
-import javafx.fxml.FXML;
-import javafx.scene.control.Label;
-import javafx.scene.control.*;
-
-public class EnergyGuiController {
-
-    @FXML
-    private Label communityLabel;
-
-    @FXML
-    private Label gridLabel;
-
-    @FXML
-    private Button refreshButton;
-
-    @FXML
-    private Button showDataButton;
-
-    @FXML
-    private DatePicker startDate;
-
-    @FXML
-    private DatePicker endDate;
-
-    @FXML private TextArea outputArea;
-
-    @FXML
-    public void initialize() {
-        refreshButton.setOnAction(e -> {
-            communityLabel.setText("Community Pool: 78.54% used");
-            gridLabel.setText("Grid Portion: 21.46%");
-        });
-
-        showDataButton.setOnAction(e -> {
-            outputArea.setText("Community produced: 143.024 kWh\n" +
-                    "Community used: 130.101 kWh\n" +
-                    "Grid used: 14.75 kWh");
-        });
-    }
-}*/
-
 package com.example.gui_javafx;
 
 import javafx.application.Platform;
@@ -56,13 +13,28 @@ import java.util.Scanner;
 
 public class EnergyGuiController {
 
-    @FXML private Label communityLabel;
-    @FXML private Label gridLabel;
-    @FXML private Button refreshButton;
-    @FXML private Button showDataButton;
-    @FXML private DatePicker startDate;
-    @FXML private DatePicker endDate;
-    @FXML private TextArea outputArea;
+    @FXML
+    private Label communityLabel;
+
+    @FXML
+    private Label gridLabel;
+
+    @FXML
+    private Button refreshButton;
+
+    @FXML
+    private Button showDataButton;
+
+
+    @FXML
+    private DatePicker startDate;
+
+    @FXML
+    private DatePicker endDate;
+
+    @FXML
+    private TextArea outputArea;
+
 
     private final HttpClient client = HttpClient.newHttpClient();
 

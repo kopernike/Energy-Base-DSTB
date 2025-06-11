e60ac62419a084878120c91a12cf4e845a557c62
Eda <wi23b191@technikum-wien.at>
2025-06-11 17:43:04 +0200

    Energy User fertig

---
+++ EnergyUser/src/main/java/com/example/EnergyUser/EnergyUserApplication.java
@@ -0,0 +1,21 @@
+package com.example.EnergyUser;
+
+import org.springframework.amqp.core.Queue;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+@SpringBootApplication
+@EnableScheduling
+public class EnergyUserApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EnergyUserApplication.class, args);
+	}
+
+@Bean
+	public Queue userQueue() {
+		return new Queue("userQueue", true);
+}
+}
---
+++ EnergyUser/src/test/java/com/example/EnergyUser/EnergyUserApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.EnergyUser;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EnergyUserApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
